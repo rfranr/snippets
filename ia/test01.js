8bc4cf14ce0655b804fb853fb0e51891c54c8446
flopez <flopez@axpe.com>
2020-11-13 09:42:27 +0100

    test

---
+++ ia/test01.js
@@ -0,0 +1,32 @@
+
+/*
+ * v1: int
+ * v2: int
+ * f:  heuristic
+*/
+function input(v1,v2, f) {
+  return ({v1:v1,v2:v2,function:f});
+}
+
+/* 
+ * envelope
+ * 
+ */
+function en() {
+
+}
+
+/* 
+ * sigma 
+ * i: input
+ * e: envelop
+ */
+function sg(i, e){
+
+}
+
+
+let n1 = input(0,1, sg);
+
+
+console.dir  ( n1 )
