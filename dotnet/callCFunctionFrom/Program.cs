8b51fced1e6bed51fcaf6a0c8f723db7e29423cf
flopez <flopez@axpe.com>
2020-11-16 11:10:08 +0100

    call a C function from C#.Net

---
+++ dotnet/callCFunctionFrom/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace callCFunctionFromc_
+{
+public class Tester
+    {
+#if DEBUG
+        public const string confDir = "Debug\\";
+#elif RELEASE
+        public const string confDir = "Release\\";
+#endif
+
+        [DllImport( confDir +  "DLL1.dll", EntryPoint="print")]
+	    public static extern void print(string message);
+
+            public static void Main(string[] args)
+            {
+		print("Hello World C# => C++");
+            }
+    }
+}
+
+
+
